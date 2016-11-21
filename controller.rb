85647d4f721543776fde2116c5e214375e785dee
Elgrogz <gregorgilchrist@gmail.com>
2016-11-21 20:15:22 +0000

    game working and basic webpage running

---
+++ controller.rb
@@ -0,0 +1,12 @@
+require('sinatra')
+require('sinatra/contrib/all')
+require_relative('models/game.rb')
+require('json')
+
+get '/play/:choice1/:choice2' do
+  choice1 = params[:choice1]
+  choice2 = params[:choice2]
+  game1 = Game.new(choice1, choice2)
+  @result = game1.play
+  erb(:result)
+end
--- models/game.rb
+++ models/game.rb
@@ -2,33 +2,49 @@ require('pry-byebug')
 
 class Game
 
-  attr_accessor :choice1, :choice2
-
   def initialize(choice1, choice2)
     @choice1 = choice1.downcase
     @choice2 = choice2.downcase
   end
 
   def play()
-      if (@choice1 || @choice2 == "rock") && (@choice1 || @choice2 == "scissors")
-        result = "Rock wins"
-      elsif (@choice1 || @choice2 == "paper") && (@choice1 || @choice2 == "rock")
-        result = "Paper wins"
-      elsif (@choice1 || @choice2 == "scissors") && (@choice1 || @choice2 == "paper")
-        result = "Scissors win"
-      else 
-        result = "Invalid Selection"
-      end
-    return result  
+    if @choice1 == "rock" && @choice2 == "scissors"
+      return "Rock wins"
+    elsif @choice1 == "scissors" && @choice2 == "rock"
+      return "Rock wins"
+    elsif @choice1 == "paper" && @choice2 == "rock"
+      return "Paper wins"
+    elsif @choice1 == "rock" && @choice2 == "paper"
+      return "Paper wins"
+    elsif @choice1 == "scissors" && @choice2 == "paper"
+      return "Scissors wins"
+    elsif @choice1 == "paper" && @choice2 == "scissors"
+      return "Scissors wins"
+    elsif @choice1 == @choice2
+      return "Draw"
+    else
+      return "Invalid inputs"
+    end
   end
 
 end
 
-game1 = Game.new("scissors", "paper")
+game1 = Game.new("Rock", "Scissors")
 puts game1.play
 
 
-    
-binding.pry
+  
 nil
 
+#original effort
+    #   if (@choice1 || @choice2 == "rock") && (@choice1 || @choice2 == "scissors")
+    #     result = "Rock wins"
+    #   elsif (@choice1 || @choice2 == "paper") && (@choice1 || @choice2 == "rock")
+    #     result = "Paper wins"
+    #   elsif (@choice1 || @choice2 == "scissors") && (@choice1 || @choice2 == "paper")
+    #     result = "Scissors win"
+    #   else 
+    #     result = "Invalid Selection"
+    #   end
+    #   binding.pry 
+    # return result 
